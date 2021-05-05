d2eb3fe002734d9bd439ce15c407389ff2e12bd0
SabeehM <sabeeh.hm@gmail.com>
2021-05-05 02:00:35 -0400

    command line color ascii

--- VODet/logger.py
+++ VODet/logger.py
@@ -1,24 +1,27 @@
 import sys
+from colorama import init, Fore, Back, Style
+init(convert = True)
+init(autoreset=True)
 class Logger:
     @staticmethod
     def print_fail(message, end = '\n'):
-        sys.stderr.write('\x1b[1;31m' + message.strip() + '\x1b[0m' + end)
+        print(Fore.RED + message, end=end)
 
     @staticmethod
     def print_pass(message, end = '\n'):
-        sys.stdout.write('\x1b[1;32m' + message.strip() + '\x1b[0m' + end)
+        print(Fore.GREEN + message, end=end)
 
     @staticmethod
     def print_warn(message, end = '\n'):
-        sys.stderr.write('\x1b[1;33m' + message.strip() + '\x1b[0m' + end)
+        print(Fore.YELLOW + message, end=end)
 
     @staticmethod
     def print_info(message, end = '\n'):
-        sys.stdout.write('\x1b[1;34m' + message.strip() + '\x1b[0m' + end)
+        print(Fore.BLUE + message, end=end)
 
     @staticmethod
     def print_bold(message, end = '\n'):
-        sys.stdout.write('\x1b[1;37m' + message.strip() + '\x1b[0m' + end)
+        print(Style.BRIGHT + message, end=end)
 
     @staticmethod
     def exit_line():
